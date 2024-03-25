199fb3bf54704d766184ce7b357ab1e6033a55e6
Thomas Ly <Thomaslyy13@gmail.com>
2024-03-25 12:32:28 -0700

    fixed chromium bug

--- scraper.js
+++ scraper.js
@@ -1,40 +1,15 @@
-const playwright = require('playwright');
+const { chromium } = require('playwright');
 
-async function scrapeWebsite(url) {
+(async () => {
 
-    // Launch the browser
-    
-    const browser = await playwright.chromium.launch();
-    
+    const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
-    
     const page = await context.newPage();
     
-    // Navigate to the website
-    
-    await page.goto(url);
-    
-    // Perform actions like clicking buttons or typing text
-    
-    // Example: await page.click(‘selector’);
-    
-    // Extract data
-    
-    // Example: const result = await page.textContent(‘selector’);
-    
-    // Close the browser
+    // go to Hacker News
+    await page.goto("https://www.google.com/");
     
     
-    
-    // Return or process the extracted data
-    
-    // Example: return result;
-    
-}
+})();
 
-scrapeWebsite("https://google.com").then(data => {
-    console.log(data);
-}).catch(e => {
-    console.error(e);
-});
 
